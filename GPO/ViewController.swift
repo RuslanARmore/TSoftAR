029cf8bee664db341b4a3c37b6cac308d99d27c2
Ruslan <rakhriev@gmail.com>
2017-10-25 22:11:32 +0700

    Initial Commit

---
+++ GPO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPO
+//
+//  Created by Руслан Ахриев on 25.10.2017.
+//  Copyright © 2017 Руслан Ахриев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
